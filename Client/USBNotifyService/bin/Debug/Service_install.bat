b0b08f367df63ecded51dd13238efd31888be3f6
fei <fei001@outlook.com>
2022-01-25 14:48:02 +0800

    1

--- Client/USBNotifyService/bin/Debug/Service_install.bat
+++
@@ -1,7 +0,0 @@
-pushd %~dp0
-
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" usbnservice.exe
-
-net start usbnsrv
-
-popd
--- Client/USBNotifyService/bin/Debug/Service_uninstall.bat
+++
@@ -1,7 +0,0 @@
-pushd %~dp0
-
-net stop usbnsrv
-
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" /u usbnservice.exe
-
-popd
