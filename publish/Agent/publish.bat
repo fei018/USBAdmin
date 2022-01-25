61da4116d09fe167884010f3c2e51a5e116eba9c
fei <fei001@outlook.com>
2022-01-25 14:43:08 +0800

    11

--- publish/Agent/publish.bat
+++
@@ -1,9 +0,0 @@
-set zip=Release.zip
-
-if exist %zip% del %zip% /q
-
-"C:\Program Files\7-Zip\7z.exe" a %zip% Setup.exe setup.ini dll
-
-copy %zip% \\hhdmstest02.hiphing.nws\USBAdmin\Update\Agent
-
-pause
