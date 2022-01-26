916285da89df5f5d577c09d0d43609338853a07a
fei <fei001@outlook.com>
2022-01-26 14:57:08 +0800

    1

--- publish/# pubilsh MVC.bat
+++
@@ -1,8 +0,0 @@
-echo aaa >> \\hhdmstest02.hiphing.nws\USBAdmin\web.config
-
-ping 127.0.0.1 -n 5 >nul
-
-
-robocopy "MVC" "\\hhdmstest02.hiphing.nws\USBAdmin" /MIR /R:0 /NP /TEE /XD Update /XF "# pubilsh MVC.bat"
-
-pause
