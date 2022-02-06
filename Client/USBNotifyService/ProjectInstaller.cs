af78d84fd5db8a7c86258f67d4fdcaa129059205
fei018 <fei001@outlook.com>
2022-02-06 23:20:25 +0800

    1

--- Client/USBNotifyService/ProjectInstaller.cs
+++ Client/USBNotifyService/ProjectInstaller.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Linq;
+using System.ServiceProcess;
 using System.Threading.Tasks;
 
 namespace USBNotifyService
@@ -14,6 +15,18 @@ namespace USBNotifyService
         public ProjectInstaller()
         {
             InitializeComponent();
+
+            this.AfterInstall += ProjectInstaller_AfterInstall;
+        }
+
+        private void ProjectInstaller_AfterInstall(object sender, InstallEventArgs e)
+        {
+            ProjectInstaller serviceInstaller = (ProjectInstaller)sender;
+
+            using (ServiceController sc = new ServiceController(serviceInstaller.serviceInstaller1.ServiceName))
+            {
+                sc.Start();
+            }
         }
     }
 }
